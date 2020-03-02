33e1a2d591cd5b862d40f7c156eaf24b1379ab88
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 18:09:00 +0000

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,9 +8,9 @@ http.createServer(function (req, res) {
 console.log('Server running at http://127.0.0.1:1337/');
 
 //Basic test to check mocha is running as intended
-var testVar = 2;
+
+
 
 function question1A() {
   var names = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
-  alert("The names of the contestants are " + names.toString());
 }
