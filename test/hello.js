40b46dcc3f5d6ef4ecd8fda7000b73ea58be0332
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 16:41:04 +0000

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -2,7 +2,8 @@
 var expect = require('chai').expect;
 
 describe('testVar', function()  {
-  it('variable should return as hello world', function() {
+  it('variable should be equal to 2', function() {
       expect(testVar).to.not.be.empty;
+      expect(testVar).to.equal.(2);
   });
 });
