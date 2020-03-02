c3346a03a951a171751d723d975ed057bd32665f
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 15:41:38 +0000

    Created first test file for mocha test
    This file should run when the travis ci build is triggered and will check if the file does have a variable of testVar and that it is not empty.

---
+++ test/hello.js
@@ -0,0 +1,8 @@
+'use strict';
+var expect = require('chai').expect;
+
+describe('testVar', function()  {
+  it('variable should return as hello world', function() {
+      expect(testVar).to.not.be.empty;
+  });
+});
