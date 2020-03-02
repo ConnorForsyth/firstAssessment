04876755d11b1185a13c8599d547be539edfeafb
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 15:38:47 +0000

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,15 +8,9 @@ http.createServer(function (req, res) {
 console.log('Server running at http://127.0.0.1:1337/');
 
 //Necessary library for Mocha framework to work
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
 //Basic test to check mocha is running as intended
 
-/*describe('testVar', function()  {
-  it('should contain the string hello world', function() {
-      var testVar = "hello world";
-      expect(testVar).to.not.be.undefined;
-      expect(testVar).to.equal.('hello world');
-  });
-});*/
 
+var testVar = "hello world";
