15a78ceb3112d4aa5518abba213d41432ec51e06
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 16:38:52 +0000

    Updated mocha test variable

--- hello.js
+++ hello.js
@@ -7,10 +7,5 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-//Necessary library for Mocha framework to work
-const expect = require('chai').expect;
-
 //Basic test to check mocha is running as intended
-
-
-var testVar = "hello world";
+var testVar = 1;
