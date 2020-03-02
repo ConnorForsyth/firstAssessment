1bef04e337a3f7d112535a86e7e744af8df431c4
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 18:37:51 +0000

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -1,16 +1,15 @@
 'use strict';
 var expect = require('chai').expect;
-var HelloJS = require('../hello.js');
+const HelloJS = require('../hello.js');
 describe('hello.js exists', function()  {
   it('should exist', function() {
       expect(HelloJS).to.not.be.undefined;
   });
 });
 
-describe('check variable', function()	{
-	it('should return an array with 5 specific names', function()	{
-		var expectedInput = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
-		
+describe('Question 1A - Array created with the celebrities names', function()	{
+	it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
+		var input = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];		
 		
 	});
 });
