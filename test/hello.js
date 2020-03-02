a802a8ed3d8321dd154016ff241a21a38d421938
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-02 18:52:15 +0000

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -1,6 +1,7 @@
 'use strict';
 var expect = require('chai').expect;
 const HelloJS = require('../hello.js');
+var celebNames = HelloJS.celebrityNames;
 describe('hello.js exists', function()  {
   it('should exist', function() {
       expect(HelloJS).to.not.be.undefined;
@@ -9,7 +10,7 @@ describe('hello.js exists', function()  {
 
 describe('Question 1A - Array created with the celebrities names', function()	{
 	it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
-		var input = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];		
-		
+		var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
+		expect(celebNames).to.eql(question1a);
 	});
 });
