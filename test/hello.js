9e2ac244d08cd6b5cf1a2b00dcfec19762bc7682
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-03 11:28:46 +0000

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -12,7 +12,10 @@ describe('hello.js exists', function()  {
 describe('Question 1A - Array created with the celebrities names', function()	{
 	it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
 		var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
+		var counter = 0;
+		console.log(counter);
 		expect(celebNames).to.eql(question1a);
+		
 	});
 });
 
