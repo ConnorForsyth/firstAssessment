492ffe754e0e50a7aeacad4bbca1f9fca8b4fd5b
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-03 11:39:09 +0000

    Added test variable to test suite
    The variable testingEnabled to check whether the student has enabled the mocha tests to run on their repositories

--- test/hello.js
+++ test/hello.js
@@ -1,26 +1,30 @@
 'use strict';
 var expect = require('chai').expect;
 const HelloJS = require('../hello.js');
+const testingEnabled = HelloJS.testing;
 var celebNames = HelloJS.testQuestion1A;
 
-describe('hello.js exists', function()  {
-  it('should exist', function() {
-      expect(HelloJS).to.not.be.undefined;
-  });
-});
-
-describe('Question 1A - Array created with the celebrities names', function()	{
-	it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
-		var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
-		var counter = 0;
-		console.log("Your score is currently: " + counter);
-		expect(celebNames).to.eql(question1a);
-		
+if(testingEnabled === true)
+{
+	describe('hello.js exists', function()  {
+	  it('should exist', function() {
+	      expect(HelloJS).to.not.be.undefined;
+	  });
 	});
-});
 
-describe('Question 1B', function()	{
-	it('An array should be created storing the number of beetles', function()	{
-		
+	describe('Question 1A - Array created with the celebrities names', function()	{
+		it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
+			var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
+			var counter = 0;
+			console.log("Your score is currently: " + counter);
+			expect(celebNames).to.eql(question1a);
+
+		});
 	});
-});
+
+	describe('Question 1B', function()	{
+		it('An array should be created storing the number of beetles', function()	{
+
+		});
+	});
+}
