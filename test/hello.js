51c8330fe897787e64f8bf097690c0f8a93ee58f
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-03 11:21:06 +0000

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -13,7 +13,7 @@ describe('Question 1A - Array created with the celebrities names', function()	{
 	it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
 		var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
 		expect(celebNames).to.eql(question1a);
-		expect(celebNames, 'Please make sure that your array contains all the required names').to.be.a('string');
+		expect(celebNames, 'Please make sure that your array contains all the required names').to.eql(question1a);
 	});
 });
 
