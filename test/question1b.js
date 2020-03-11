563dbd401aa01f93d4e62ae5df0015a13a13b17a
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-11 14:51:34 +0000

    Update question1b.js
    Changed the testingEnabled variable to match changes made in hello.js

--- test/question1b.js
+++ test/question1b.js
@@ -2,7 +2,7 @@
 var expect = require('chai').expect;
 const HelloJS = require('../hello.js');
 const testingEnabled = HelloJS.testing;
-var celebNames = HelloJS.testQuestion1A;
+var celebNames = HelloJS.question1bTest;
 
 describe('Question 1B', function()	{
 		it('An array should be created storing the number of beetles', function()	{
