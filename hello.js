601133bd39eaf1b33bcc5e58440f420682db648e
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-11 16:11:17 +0000

    Update hello.js
    Added beetles export to test question1b

--- hello.js
+++ hello.js
@@ -12,7 +12,7 @@ console.log('Server running at http://127.0.0.1:1337/');
 //Here you can able testing for the various questions you will attempt
 //To test set the variable to true, otherwise set to false
 module.exports.question1aTest = true;
-module.exports.question1bTest = false;
+module.exports.question1bTest = true;
 
 //Question 1A
 var celebrityNames = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"];
@@ -29,6 +29,9 @@ for (let i = 0; i < beetles.length; i++){
 }
 
 console.log(beetles.toString());
+
+//To test whether your beetles array is correct, you will need to declare the variables in this export function
+module.exports.testBeetlesArray = beetles;
 
 var min = beetles[0];
 var personIndex = 0;
