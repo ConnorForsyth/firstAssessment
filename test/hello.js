cdc8947e900a6c032da701fe2ccb644012470b6d
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-11 12:11:28 +0000

    Update hello.js
    Removed typo from celebNames conversion

--- test/hello.js
+++ test/hello.js
@@ -15,7 +15,7 @@ if(testingEnabled === true)
 	describe('Question 1A - Array created with the celebrities names', function()	{
 		it('Names array should contain Flash, Anne, Chelsee, Corrie, Des and Zed', function()	{
 			var question1a = ["Flash", "Anne", "Chelsee", "Corrie", "Des", "Zed"].map(v => v.toLowerCase());
-			celebNames = celebNames..map(v => v.toLowerCase());
+			celebNames = celebNames.map(v => v.toLowerCase());
 			var questionScore = 0;
 			if(expect(celebNames).to.eql(question1a)){questionScore=2;}
 			else{questionScore = 0;}
