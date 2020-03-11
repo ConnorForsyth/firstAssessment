f7a452f8ccf87917cefdad9e3b042674c28ae25c
ConnorForsyth <connorforsyth34@gmail.com>
2020-03-11 12:47:16 +0000

    Update question1b.js

--- test/question1b.js
+++ test/question1b.js
@@ -1,3 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+const HelloJS = require('../hello.js');
+const testingEnabled = HelloJS.testing;
+var celebNames = HelloJS.testQuestion1A;
+
 describe('Question 1B', function()	{
 		it('An array should be created storing the number of beetles', function()	{
 
